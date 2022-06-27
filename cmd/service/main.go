592f81f973f7d198f3266b68fbcaeb90f8500f98
cryshado <mirmiclub@gmail.com>
2022-06-27 16:08:33 +0300

    add LICENSE

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,5 +1,21 @@
 package main
 
+// TON Blockchain smart contracts event indexing service
+// Copyright (C) 2022 https://github.com/cryshado
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import (
 	"encoding/json"
 	"fmt"
@@ -50,7 +66,7 @@ func main() {
 
 	fmt.Println(prettyPrint(resp))
 	for {
-		logrus.Debug("ABOfA")
+		logrus.Debug("test debug msg")
 		time.Sleep(1 * time.Second)
 	}
 }
