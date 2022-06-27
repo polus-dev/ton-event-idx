b3a6e7b302c1b396d4d114296b86a0c846ebd2a8
cryshado <mirmiclub@gmail.com>
2022-06-27 16:12:58 +0300

    update license header

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,20 +1,23 @@
-//
-// TON Blockchain smart contracts event indexing service
-// Copyright (C) 2022 https://github.com/cryshado
+/*
+	ton-event-idx – TON Blockchain smart contracts event indexing service
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+	Copyright (C) 2022 https://github.com/cryshado
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+	This file is part of ton-event-idx.
 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
+	ton-event-idx is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ton-event-idx is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with ton-event-idx.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 package main
 
