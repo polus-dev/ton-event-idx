33dd78eabcc1185409f81fb0f3a22019f52af958
cryshado <mirmiclub@gmail.com>
2022-07-17 15:26:21 +0300

    new sql

--- sql/new.sql
+++ sql/new.sql
@@ -1,11 +1,42 @@
 DROP TABLE IF EXISTS mc_block CASCADE;
+DROP TABLE IF EXISTS block CASCADE;
+DROP TABLE IF EXISTS contract CASCADE;
+DROP TABLE IF EXISTS event CASCADE;
 
 CREATE TABLE mc_block
+( ------------ work_chain always -1 (master chain)
+    id         UUID      NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    shard      BYTEA     NOT NULL,
+    seqno      BIGINT    NOT NULL
+);
+
+CREATE TABLE block
 (
-    id         BYTEA PRIMARY KEY NOT NULL,
-    work_chain INTEGER           NOT NULL,
-    shard      BYTEA             NOT NULL,
-    seqno      BIGINT            NOT NULL,
-    root_hash  BYTEA             NOT NULL,
-    file_hash  BYTEA             NOT NULL
+    id          UUID      NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    mc_block_id UUID      NOT NULL REFERENCES mc_block (id),
+
+    work_chain  INTEGER   NOT NULL,
+    shard       BYTEA     NOT NULL,
+    seqno       BIGINT    NOT NULL
+);
+
+
+CREATE TABLE contract
+(
+    id         SERIAL UNIQUE PRIMARY KEY NOT NULL,
+    work_chain BYTEA                     NOT NULL,
+    address    BYTEA                     NOT NULL
+);
+
+CREATE TABLE event
+(
+    id          UUID      NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    block_id    UUID      NOT NULL REFERENCES block (id),
+    contract_id INTEGER   NOT NULL REFERENCES contract (id),
+
+    body        BYTEA     NOT NULL
 );
